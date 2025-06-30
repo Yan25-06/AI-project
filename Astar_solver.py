eefb60f26a79f163a9f456b5208073ebdbb6e933
Yan25-06 <huytoan709@gmail.com>
2025-06-30 17:11:54 +0700

    .

--- Astar_solver.py
+++ Astar_solver.py
@@ -2,3 +2,6 @@ from solver_base import Solver, Node
 
 class AstarSolver(Solver):
     def solve(self): ...
+    # if is_goal
+    #   self.solution = _reconstruct_path 
+    #   ...
--- DFS_solver.py
+++ DFS_solver.py
@@ -2,3 +2,6 @@ from solver_base import Solver, Node
 
 class DFSSolver(Solver):
     def solve(self): ...
+    # if is_goal
+    #   self.solution = _reconstruct_path 
+    #   ...
